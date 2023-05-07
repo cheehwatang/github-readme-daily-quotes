790c4104676fc69ed9a3023275b7b5916141c453
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-05-08 01:53:02 +0800

    Adds basic style to the card

--- src/renderQuoteCard.ts
+++ src/renderQuoteCard.ts
@@ -1,6 +1,6 @@
 const renderQuoteCard = (): string => {
   const renderedSVG = `
-    <svg width="500" height="auto" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="500" height="200" fill="none" xmlns="http://www.w3.org/2000/svg">
       <foreignObject width="100%" height="100%">
         <div xmlns="http://www.w3.org/1999/xhtml">
           <style>
@@ -11,30 +11,64 @@ const renderQuoteCard = (): string => {
             }
             .card {
               font-family: Helvetica, sans-serif;
-              padding: 20px;
+              background-color: #0d1117;
+              padding: 18px;
               width: 500px;
-              border: 2px solid rgba(0, 0, 0);
-              border-radius: 4px;
+              height: 200px;
+              border: 2px solid #c3d1d9;
+              border-radius: 12px;
+              display: flex;
+              flex-direction: column;
+              justify-content: space-around;
             }
             .quote {
-              margin-bottom: 1rem;
+              font-size: 1.2rem;
+              color: #c3d1d9;
+              padding: 0 1rem;
             }
-            .author {
-              font-style: italic;
+            .quote::before,
+            .quote::after {
+              display: block;
+              font-size: 2.4rem;
+              color: #58a6ff;
+              margin-bottom: -1rem;
             }
             .quote::before {
               content: open-quote;
+              text-align: left;
+              transform: translateX(-1.2rem);
             }
             .quote::after {
               content: close-quote;
+              text-align: right;
+              transform: translateX(1.2rem);
             }
-            .quote::before, .quote::after {
-              display:block;
+            .author {
+              font-size: 0.8rem;
+              font-style: italic;
+              color: #c3d1d9;
+            }
+            .author::before {
+              display: block;
+              content: '';
+              width: 1.5rem;
+              border-bottom: solid 3px #58a6ff;
+              margin-bottom: 1rem;
+            }
+            .text-concat {
+              display: -webkit-box;
+              -webkit-line-clamp: 3;
+              -webkit-box-orient: vertical;
+              overflow: hidden;
             }
           </style>
           <div class="card">
-            <h1 class="quote">Life is long, if you know how to use it.</h1>
-            <p class="author">- Seneca</p>
+            <h1 class="quote">
+              <span class="text-concat">
+                Life is long, if you know how to use it.
+              </span>
+            </h1>
+            <p class="author">Seneca</p>
           </div>
         </div>
       </foreignObject>
