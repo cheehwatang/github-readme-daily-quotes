0ae3000f251a7aaa3f69c8b26b1c1800633ed65d
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-05-11 19:10:41 +0800

    Accepts color settings on the query string

--- api/index.ts
+++ api/index.ts
@@ -7,10 +7,24 @@ interface Query {
   quote: string;
   author: string;
   theme: string;
+  bg_color: string;
+  quote_color: string;
+  author_color: string;
+  accent_color: string;
+  border_color: string;
 }
 
 const handler = async (request: VercelRequest, response: VercelResponse) => {
-  const { quote, author, theme } = request.query as unknown as Query;
+  const {
+    quote,
+    author,
+    theme,
+    bg_color,
+    quote_color,
+    author_color,
+    accent_color,
+    border_color,
+  } = request.query as unknown as Query;
 
   const quoteData: QuoteData = await setQuoteData({ quote, author });
 
