8417dfb95eca1e01391b15292ac93afd5fdf8bc9
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-05-12 15:44:40 +0800

    Set max quote length of stoicism quote

--- src/services/fetchStoicismQuote.ts
+++ src/services/fetchStoicismQuote.ts
@@ -1,13 +1,22 @@
 import axios from 'axios';
 import { QuoteData } from '../utils/setQuoteData';
 import { fetchDefaultQuote } from './fetchDefaultQuote';
+import { CONSTANTS } from '../config';
 
 const apiUrl = 'https://api.themotivate365.com/stoic-quote';
 
 const fetchStoicismQuote = async (): Promise<QuoteData> => {
   try {
     const response = await axios.get(apiUrl);
-    return response.data as QuoteData;
+
+    let quote = response.data as QuoteData;
+    while (quote.quote.length > CONSTANTS.MAX_QUOTE_LENGTH) {
+      const response = await axios.get(apiUrl);
+
+      quote = response.data as QuoteData;
+    }
+
+    return quote;
   } catch (error: unknown) {
     return fetchDefaultQuote();
   }
