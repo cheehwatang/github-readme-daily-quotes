7daa77d638c8d3b2bb1b85d01c48a9c27e4e9dff
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-05-12 20:19:46 +0800

    Adds feature to accept font parameter for web safe fonts

--- src/renderQuoteCard.ts
+++ src/renderQuoteCard.ts
@@ -2,8 +2,12 @@ import { QuoteCard } from './templates/QuoteCard';
 import { QuoteData } from './utils/setQuoteData';
 import { Theme } from './utils/setTheme';
 
-const renderQuoteCard = (data: QuoteData, theme: Theme): string => {
-  return QuoteCard.build(data, theme);
+const renderQuoteCard = (
+  data: QuoteData,
+  theme: Theme,
+  fontFamily: string
+): string => {
+  return QuoteCard.build(data, theme, fontFamily);
 };
 
 export { renderQuoteCard };
---
+++ src/utils/setFont.ts
@@ -0,0 +1,5 @@
+const setFont = (font: string): string => {
+  return font;
+};
+
+export { setFont };
