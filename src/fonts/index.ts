442e90ccc544584f6c05c9e71b98c19cf165dce1
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-05-12 20:43:30 +0800

    Adds web safe font families

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -3,6 +3,11 @@ const fontFamilies: { [key: string]: string } = {
   arial: 'Arial, sans-serif',
   verdana: 'Verdana, sans-serif',
   tahoma: 'Tahoma, sans-serif',
+  trebuchet_ms: "'Trebuchet MS', sans-serif",
+  times_new_roman: "'Times New Roman', serif",
+  georgia: 'Georgia, serif',
+  garamond: 'Garamond, serif',
+  courier_new: "'Courier New', monospace",
 };
 
 export { fontFamilies };
