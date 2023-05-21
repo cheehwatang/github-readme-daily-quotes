f664d477315fc0d6328bfe8b5911754f05e1abe1
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-05-21 20:55:01 +0800

    Lint typescript files

--- src/services/fetchDailyQuote.ts
+++ src/services/fetchDailyQuote.ts
@@ -3,24 +3,25 @@ import { QuoteData } from '../utils/setQuoteData';
 import { fetchDefaultQuote } from './fetchDefaultQuote';
 
 type DailyQuote = {
-  q: string;
-  a: string;
+	q: string;
+	a: string;
 };
 
 const apiUrl = 'https://zenquotes.io/api/today';
 
 const fetchDailyQuote = async (): Promise<QuoteData> => {
-  try {
-    const response = await axios.get(apiUrl);
+	try {
+		const response = await axios.get(apiUrl);
+		const data = response.data as DailyQuote[];
 
-    return parseData(response.data[0] as DailyQuote);
-  } catch (error: unknown) {
-    return fetchDefaultQuote();
-  }
+		return parseData(data[0]);
+	} catch (error: unknown) {
+		return fetchDefaultQuote();
+	}
 };
 
 const parseData = (data: DailyQuote): QuoteData => {
-  return { quote: data.q, author: data.a };
+	return { quote: data.q, author: data.a };
 };
 
 export { fetchDailyQuote };
--- src/utils/setTheme.ts
+++ src/utils/setTheme.ts
@@ -2,60 +2,60 @@ import { themes } from '../themes';
 import { defaultTheme, borderWidthConfig, borderRadiusConfig } from '../config';
 
 type Theme = {
-  bg_color: string;
-  quote_color: string;
-  author_color: string;
-  accent_color: string;
-  border_color: string;
-  border_width?: number;
-  border_radius?: number;
+	bg_color: string;
+	quote_color: string;
+	author_color: string;
+	accent_color: string;
+	border_color: string;
+	border_width: number;
+	border_radius: number;
 };
 
 const setTheme = (theme: string, customTheme: Theme): Theme => {
-  const themeData = themes[theme] || defaultTheme;
+	const themeData = themes[theme] || defaultTheme;
 
-  for (const key in customTheme) {
-    customTheme[key] ||= themeData[key];
-  }
+	for (const key in customTheme) {
+		customTheme[key] ||= themeData[key];
+	}
 
-  setBorderWidth(customTheme);
-  setBorderRadius(customTheme);
+	setBorderWidth(customTheme);
+	setBorderRadius(customTheme);
 
-  return customTheme;
+	return customTheme;
 };
 
 const setBorderWidth = (theme: Theme): void => {
-  if (
-    !theme.border_width ||
-    !isValueWithinRange(
-      theme.border_width,
-      borderWidthConfig.min,
-      borderWidthConfig.max
-    )
-  ) {
-    theme.border_width = borderWidthConfig.default;
-  }
+	if (
+		!theme.border_width ||
+		!isValueWithinRange(
+			theme.border_width,
+			borderWidthConfig.min,
+			borderWidthConfig.max
+		)
+	) {
+		theme.border_width = borderWidthConfig.default;
+	}
 };
 
 const setBorderRadius = (theme: Theme): void => {
-  if (
-    !theme.border_radius ||
-    !isValueWithinRange(
-      theme.border_radius,
-      borderRadiusConfig.min,
-      borderRadiusConfig.max
-    )
-  ) {
-    theme.border_radius = borderRadiusConfig.default;
-  }
+	if (
+		!theme.border_radius ||
+		!isValueWithinRange(
+			theme.border_radius,
+			borderRadiusConfig.min,
+			borderRadiusConfig.max
+		)
+	) {
+		theme.border_radius = borderRadiusConfig.default;
+	}
 };
 
 const isValueWithinRange = (
-  value: number,
-  min: number,
-  max: number
+	value: number,
+	min: number,
+	max: number
 ): boolean => {
-  return value >= min && value <= max;
+	return value >= min && value <= max;
 };
 
 export { setTheme, Theme };
