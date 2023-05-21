91e26782236983023bd7dde34d58b08a05fcfe9c
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-05-21 20:35:14 +0800

    Eslint overrides for test files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,14 @@
+/* eslint-env node */
 module.exports = {
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	overrides: [
+		{
+			files: '**/*.test.*',
+			rules: {
+				'@typescript-eslint/no-unsafe-call': 'off',
+			},
+		},
+	],
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
 	root: true,
