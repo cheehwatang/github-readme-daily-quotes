52add4d0b5e92be90e376a36d5f74f20579124f9
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-05-21 20:45:15 +0800

    Adds type linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,10 @@
 /* eslint-env node */
 module.exports = {
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+	],
 	overrides: [
 		{
 			files: '**/*.test.*',
@@ -8,8 +12,18 @@ module.exports = {
 				'@typescript-eslint/no-unsafe-call': 'off',
 			},
 		},
+		{
+			extends: [
+				'plugin:@typescript-eslint/recommended-requiring-type-checking',
+			],
+			files: ['**/*.ts'],
+		},
 	],
-	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: true,
+		tsconfigRootDir: __dirname,
+	},
 	root: true,
 };
